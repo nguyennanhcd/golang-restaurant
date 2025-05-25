f3b99e74370328ffcb952353aa72a127da2f6fd2
nguyenanhcd <anh487303@gmail.com>
2025-05-25 10:21:49 +0700

    sorry

--- controller/tableController.go
+++ controller/tableController.go
@@ -1,10 +1,43 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var tableCollection *mongo.Collection = database.OpenCollection(database.Client, "tables")
 
 func GetTables() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := tableCollection.Find(ctx, bson.D{})
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Error occurred while fetching tables"})
+			return
+		}
+
+		var allTables []bson.M
+		if err = result.All(ctx, &allTables); err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		if len(allTables) == 0 {
+			c.JSON(404, gin.H{"message": "No orders found"})
+			return
+		}
+
+		c.JSON(200, allTables)
 	}
 }
 
