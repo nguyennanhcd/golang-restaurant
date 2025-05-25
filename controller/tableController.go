2e2702c3a150cdb5de009a536184d0fcc5393666
nguyenanhcd <anh487303@gmail.com>
2025-05-25 10:41:28 +0700

    done table controller

--- controller/tableController.go
+++ controller/tableController.go
@@ -3,11 +3,14 @@ package controller
 import (
 	"context"
 	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -43,12 +46,55 @@ func GetTables() gin.HandlerFunc {
 
 func GetTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		orderID := c.Param("table_id")
 
+		var table models.Table
+
+		err := foodCollection.FindOne(ctx, bson.M{"table_id": orderID}).Decode(&table)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while fetching table"})
+			return
+		}
+
+		c.JSON(http.StatusOK, table)
 	}
 }
 
 func CreateTable() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var table models.Table
+
+		if err := c.BindJSON(&table); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(table)
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		table.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		table.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+		table.ID = primitive.NewObjectID()
+		table.Table_id = table.ID.Hex()
+
+		result, insertErr := tableCollection.InsertOne(ctx, table)
+		if insertErr != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occurred while creating table"})
+			return
+		}
+
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 
 	}
 }
