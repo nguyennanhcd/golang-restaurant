72c670b3cccf6401a4a48b0c6ee47baee65ab5c0
nguyenanhcd <anh487303@gmail.com>
2025-05-29 10:27:43 +0700

    done reviewcode

--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -12,7 +12,7 @@ type Invoice struct {
 	Order_id         string             `json:"order_id"`
 	Payment_method   *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
 	Payment_status   *string            `json:"payment_status" validate:"required,eq=PAID|eq=PENDING|eq=FAILED"`
-	Payment_due_date time.Time          `json:"payment_due"`
+	Payment_due_date time.Time          `json:"payment_due_date"`
 	Created_at       time.Time          `json:"created_at"`
 	Updated_at       time.Time          `json:"updated_at"`
 }
